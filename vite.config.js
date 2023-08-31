ea8721eaa2a0fc6492fda79bc7ec5b280426bf45
giuseppelamaestra <giuseppe.la.maestra@gmail.com>
2023-08-31 20:38:50 +0200

    other stuff added

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,14 @@
 import { defineConfig } from 'vite';
-const path = require('path')
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.scss',
+                'resources/sass/app.scss',
                 'resources/js/app.js',
             ],
             refresh: true,
         }),
     ],
-
-    resolve: {
-        alias: {
-            '~resources' : '/resources/',
-            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-        }
-    }
 });
